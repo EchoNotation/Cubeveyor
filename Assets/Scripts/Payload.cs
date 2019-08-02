e65e1126c5183bec7b6979346ef54f6fcbe433cf
Thomas <thetardis1968@gmail.com>
2019-08-02 18:16:55 -0500

    Preliminary file structure and basic pusher/payload logic

---
+++ Assets/Scripts/Payload.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Payload : MonoBehaviour
+{
+    //The direction this cube is travelling
+    private Rigidbody body;
+    private Direction direction;
+    private bool isGrappled;
+    private const int forceConstant = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrappled = false;
+        body = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGrappled)
+        {
+            body.velocity = new Vector3();
+        }
+    }
+
+    public void Grab(Direction dir)
+    {
+        isGrappled = true;
+        direction = dir;
+    }
+
+    public void Release()
+    {
+        isGrappled = false;
+
+        Vector3 force = new Vector3();
+
+        switch(direction)
+        {
+            case Direction.UP:
+                force = Vector3.up;
+                break;
+            case Direction.DOWN:
+                force = Vector3.down;
+                break;
+            case Direction.LEFT:
+                force = Vector3.left;
+                break;
+            case Direction.RIGHT:
+                force = Vector3.right;
+                break;
+            case Direction.FORWARD:
+                force = Vector3.forward;
+                break;
+            case Direction.BACKWARD:
+                force = Vector3.back;
+                break;
+            default:
+                Debug.Log("Unrecognizaed payload direction! Direction: " + direction.ToString());
+                break;
+        }
+
+        body.AddForce(force * forceConstant, ForceMode.Impulse);
+
+    }
+}
---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    FORWARD,
+    BACKWARD,
+    BLANK,
+}
+
+public class Pusher : MonoBehaviour
+{
+    //The direction that this pusher imparts its  force in.
+    private System.Diagnostics.Stopwatch timer;
+    public Direction direction;
+    private bool waitingToDeploy;
+    private const long deployTimer = 1000;
+    private long initialTime;
+    private GameObject grabbedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitingToDeploy = false;
+        timer = new System.Diagnostics.Stopwatch();
+        timer.Start();
+        initialTime = timer.ElapsedMilliseconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(waitingToDeploy)
+        {
+            if(timer.ElapsedMilliseconds - deployTimer > initialTime)
+            {
+                //Deploy the payload!
+                grabbedObject.GetComponent<Payload>().Release();
+                initialTime = timer.ElapsedMilliseconds;
+                waitingToDeploy = false;
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.CompareTag("Payload"))
+        {
+            grabbedObject = col.gameObject;
+            grabbedObject.GetComponent<Payload>().Grab(direction);
+            waitingToDeploy = true;
+        }
+    }
+
+}
