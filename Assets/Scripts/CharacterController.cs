378ca602e62aa072379914a8f93485700b94c8ec
Emilio Alvarez <e.l.alvarez@icloud.com>
2019-08-03 12:07:58 -0500

    Grab and Drop objects

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -15,12 +15,20 @@ public class CharacterController : MonoBehaviour
     public float speed = 10.0f;
     private float translation;
     private float straffe;
+    public Camera playerCam;
+    public float rayDistance;
+    private GameObject objectHeld;
+    private bool isObjectHeld;
+    public float distance = 1f;
+    public float maxDistanceGrab = 100f;
 
     // Use this for initialization
     void Start()
     {
         // turn off the cursor
         Cursor.lockState = CursorLockMode.Locked;
+        playerCam = GetComponentInChildren<Camera>();
+        isObjectHeld = false;
     }
 
     // Update is called once per frame
@@ -32,10 +40,56 @@ public class CharacterController : MonoBehaviour
         straffe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         transform.Translate(straffe, 0, translation);
 
-        if (Input.GetKeyDown("escape"))
+
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             // turn on the cursor
             Cursor.lockState = CursorLockMode.None;
         }
+
+        if(Input.GetKeyDown(KeyCode.Mouse1)){
+            if(!isObjectHeld && objectHeld == null){
+                Pickup();
+            } else if(isObjectHeld){
+                DropObject();
+            }
+        } else if (objectHeld != null){
+            holdObject();
+        }
+    }
+
+    void Pickup()
+    {
+        RaycastHit hit;
+        Ray ray = playerCam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            if (hit.collider.tag == "Pickupable")
+            {
+                objectHeld = hit.collider.gameObject;
+                isObjectHeld = true;
+            }
+        }
+    }
+      private void holdObject(){
+        Ray playerAim = playerCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+       
+        Vector3 nextPos = playerCam.transform.position + playerAim.direction * distance;
+        Vector3 currPos = objectHeld.transform.position;
+               
+        objectHeld.GetComponent<Rigidbody>().velocity = (nextPos - currPos) * 10;
+       
+        if (Vector3.Distance(objectHeld.transform.position, playerCam.transform.position) > maxDistanceGrab)
+        {
+            DropObject();
+        }
+    }
+   
+    private void DropObject()
+    {
+        isObjectHeld = false;
+        objectHeld.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        objectHeld = null;
     }
 }
