f5510b6e4ec9916c1f522bfcaa9b0050a62ad0d9
silasobey <>
2019-08-02 23:33:11 -0500

    (bad) code for player movement and look
    committing so thomas can easily access and work on it.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody body;
+    public float sensitivityX;
+    private float rotationX = 0.0f;
+    private float mouseX;
+    private Vector3 forward;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = this.GetComponent<Rigidbody>();
+        forward = Vector3.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            body.AddForce(forward);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 temp = Quaternion.AngleAxis(-90, Vector3.up) * forward;
+            body.AddForce(temp);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 temp = Quaternion.AngleAxis(90, Vector3.up) * forward;
+            body.AddForce(temp);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            body.AddForce(-forward);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            body.AddForce(Vector3.down);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.AddForce(Vector3.up);
+        }
+    }
+
+    public void UpdateForward(Vector3 newAngle)
+    {
+        forward = new Vector3(newAngle.x, newAngle.y, 0);
+    }
+}
--- Assets/Scripts/PlayerCameraLook.cs
+++ Assets/Scripts/PlayerCameraLook.cs
@@ -2,31 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLookScript : MonoBehaviour
+public class PlayerCameraLook : MonoBehaviour
 {
-    public float sensitivityY;
-    public float sensitivityX;
-
-    private float rotationX = 0.0f;
+    public float sensitivityY, sensitivityX;
     private float rotationY = 0.0f;
-    private float mouseX;
-    private float mouseY;
+    private float rotationX = 0.0f;
+    private float mouseY, mouseX;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sensitivityX = 100;
+        sensitivityY = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mouseX = Input.GetAxis("Mouse X");
         mouseY = Input.GetAxis("Mouse Y");
-
+        mouseX = Input.GetAxis("Mouse X");
         rotationX += mouseX * sensitivityX * Time.deltaTime;
         rotationY += -mouseY * sensitivityY * Time.deltaTime;
 
         Quaternion finalRotation = Quaternion.Euler(rotationY, rotationX, 0.0f);
-        transform.rotation = finalRotation;
+        this.GetComponentInChildren<Transform>().rotation = finalRotation;
+        Debug.Log("Cameraside: " + this.GetComponentInChildren<Transform>().rotation.eulerAngles.x);
     }
 }
