82b63d10247e4328c32b27b398db75241e6d726a
Thomas <thetardis1968@gmail.com>
2019-08-03 13:50:57 -0500

    Renamed main menu buttons and added some transition logic

---
+++ Assets/Scripts/LightsOn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsOn : MonoBehaviour
+{
+    GameObject[] lights;
+    System.Diagnostics.Stopwatch timer;
+    private const int lightDelay = 1000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lights = GameObject.FindGameObjectsWithTag("Light");
+
+        foreach(GameObject i in lights)
+        {
+            i.GetComponent<Light>().enabled = false;
+        }
+
+        timer.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer.ElapsedMilliseconds > lightDelay)
+        {
+            foreach(GameObject i in lights)
+            {
+                i.GetComponent<Light>().enabled = true;
+            }
+
+            GameObject.Find("SoundManager").GetComponent<SoundManager>().LightsSound();
+
+            timer.Stop();
+            timer.Reset();
+        }
+    }
+}
