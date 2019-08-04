089a94f41243bd9728c961075d0942d0d31f80aa
Emilio Alvarez <e.l.alvarez@icloud.com>
2019-08-04 14:04:57 -0500

    Wait before passing to next level

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -6,25 +6,32 @@ public class Goal : MonoBehaviour
 {
     public Transform sceneController;
 
+    private System.Diagnostics.Stopwatch timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = new System.Diagnostics.Stopwatch();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       if (timer.ElapsedMilliseconds > 1000) {
+            Variables.lastLevel++;
+            sceneController.GetComponent<SceneControl>().LoadScene("Level Transition");
+        } 
     }
 
     public void OnCollisionEnter(Collision col)
     {
-        if(col.gameObject.CompareTag("Payload"))
+        if(col.gameObject.CompareTag("Payload") && !timer.IsRunning)
         {
             //Complete level
-            Variables.lastLevel++;
-            sceneController.GetComponent<SceneControl>().LoadScene("Level Transition");
+            timer.Reset();
+            timer.Start();
         }
+        
+    
     }
 }
